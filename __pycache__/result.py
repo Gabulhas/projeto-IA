6aeef7726bc28c610737dee722456828917f1ffc
Guilherme Lopes <guilhermebidarra@gmail.com>
2020-12-22 23:32:34 +0000

    first commit

---
+++ __pycache__/result.py
@@ -0,0 +1,55 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 3.8 (3413)
+# Decompiled from: Python 3.8.6 (default, Sep 30 2020, 04:00:38) 
+# [GCC 10.2.0]
+# Embedded file name: /root/agente.py
+# Compiled at: 2020-11-08 12:36:50
+# Size of source mod 2**32: 835 bytes
+"""
+agente.py
+
+criar aqui as funções que respondem às perguntas
+e quaisquer outras que achem necessário criar
+
+colocar aqui os nomes e número de aluno:
+NUM1, NOME1
+NUM2, NOME2
+
+"""
+import time
+
+def work(posicao, bateria, objetos):
+    pass
+
+
+def resp1():
+    pass
+
+
+def resp2():
+    pass
+
+
+def resp3():
+    pass
+
+
+def resp4():
+    pass
+
+
+def resp5():
+    pass
+
+
+def resp6():
+    pass
+
+
+def resp7():
+    pass
+
+
+def resp8():
+    pass
+# okay decompiling agente.cpython-38.pyc
